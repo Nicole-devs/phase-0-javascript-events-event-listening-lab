28dc49ccec1b895413c6e548a3ef137d676b032a
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-04-13 15:21:41 +0300

    Done

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Method 1
+function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
+}
+// Method 2
 function addingEventListener() {
     const input = document.getElementById('input');
 
